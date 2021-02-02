cd2b6e25435f30291b0454be63c5b59a5cf67d56
JeongheonKim <kimkjh0645@gmail.com>
2021-02-03 01:06:51 +0900

    modifid Classification_flowers

--- tensorflow_tutorial/Classification_flowers.py
+++ tensorflow_tutorial/Classification_flowers.py
@@ -1,25 +1,68 @@
 import matplotlib.pyplot as plt
+import matplotlib.image as img
 import numpy as np
 import os
 import PIL
 import tensorflow as tf
 import time
 
-
-
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
 import pathlib
-startTime = time.time()
-
+# Data Load
 dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
 data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
-data_dir = pathlib.Path(data_dir)
-
-endTime = time.time() - startTime
-print(endTime) 
+# https://ryanking13.github.io/2018/05/22/pathlib.html
+data_dir = pathlib.Path(data_dir)  
 
 image_count = len(list(data_dir.glob('*/*.jpg')))
 print(image_count)
+
+roses = list(data_dir.glob('roses/*'))
+rose =  img.imread(str(roses[0]))   # (height = 240, width = 170, RGB = 3)
+plt.imshow(rose)
+PIL.Image.open(str(roses[1])).show()
+
+batch_size = 32
+img_height = 180
+img_width = 180
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,   
+  validation_split=0.2,
+  subset="training",
+  seed=123,                             
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+
+class_names = train_ds.class_names
+print(class_names)
+
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+  for i in range(9):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.title(class_names[labels[i]])
+    plt.axis("off")
+
+plt.show()
+
+
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
