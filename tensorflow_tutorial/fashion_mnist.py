c3351d10a61ba43368e17db460655e6d4e523aa5
JeongheonKim <kimkjh0645@gmail.com>
2021-02-02 18:14:46 +0900

    tensorflow_test

--- tensorflow_tutorial/fashion_mnist.py
+++ tensorflow_tutorial/fashion_mnist.py
@@ -6,39 +6,32 @@ import matplotlib.pyplot as plt
 
 fashion_mnist = keras.datasets.fashion_mnist
 
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+# Data split [Training Set, Test Set]
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data() 
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-train_images = train_images / 255.0 # Normalization (0 ~ 1) : 학습의 속도와 local optimum 에 빠지지 않게 하기 위해 사용
+# Normalization (0 ~ 1) : 학습의 속도와 local optimum 에 빠지지 않게 하기 위해 사용
+train_images = train_images / 255.0 
 test_images = test_images / 255.0
 
-
-class MyModel(Model):
-      def __init__(self):
-    super().__init__()
-    self.conv1 = Conv2D(32, 3, activation='relu')
-    self.keras.layers.Flatten(input_shape=(28, 28)
-    self.flatten = Flatten()
-    self.d1 = Dense(128, activation='relu')
-    self.d2 = Dense(10, activation='softmax')
-
-  def call(self, x):
-    x = self.conv1(x)
-    x = self.flatten(x)
-    x = self.d1(x)
-    return self.d2(x)
-
-model = MyModel()
-
+# Create Model 
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),    
+    keras.layers.Flatten(input_shape=(28, 28)),     # 
     keras.layers.Dense(128, activation='relu'),
-    keras.layers.Dense(10, activation='softmax')
+    keras.layers.Dense(10, activation='softmax')    # SoftMax를 마지막 활성함수로 사용
 ])
 
 model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
+              loss='sparse_categorical_crossentropy', 
               metrics=['accuracy'])
+
+model.summary()
+
+model.fit(train_images, train_labels, epochs=5, verbose=0 )
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('\n테스트 정확도:', test_acc)
 
